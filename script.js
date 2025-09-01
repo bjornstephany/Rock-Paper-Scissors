56153fb16af10fd182fe49dfa02c9c117aa95c86
bjorn <bjornstephany@gmail.com>
2025-09-01 16:11:59 +0200

    create computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+//Write logic to get the computer choice
+//Create new function named getComputer Choice
+function getComputerChoice() {
+    let num = Math.random();
+    let choice;
+    if (num > 0.6666666666) {
+        choice = "rock";
+    } else if (num <=0.666666666 && num > 0.3333333333) {
+        choice = "paper";
+    } else {
+        choice = "scissors";
+    }
+    return choice;
+}
+
+console.log(getComputerChoice());
