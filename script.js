72bfdd242aad978119fd727c1ff4651872e575c7
bjorn <bjornstephany@gmail.com>
2025-10-21 10:53:45 +0200

    add dom manipulation

--- script.js
+++ script.js
@@ -1,54 +1,57 @@
+//Declare the players score variables
+let humanScore = 0;
+let computerScore = 0;
+
+//Write the logic to play the entire game
+function playRound() {
+  let computerChoice = getComputerChoice();
+  if (humanChoice == "rock" && computerChoice == "scissors") {
+    humanScore++;
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    humanScore++;
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    humanScore++;
+  } else {
+    computerScore++;
+  }
+  document.getElementById("humanScore").innerHTML = "Human Score: " + humanScore;
+  document.getElementById("computerScore").innerHTML = "Computer Score: " + computerScore;
+  if(humanScore == 5){
+    alert("You win!")
+  }
+  if(computerScore == 5) {
+    alert("You lose!")
+  }
+}
+let humanChoice;
+const rock = document.querySelector("#Rock");
+rock.addEventListener("click", () => {
+  humanChoice = "rock";
+  playRound();
+});
+
+const paper = document.querySelector("#Paper");
+paper.addEventListener("click", () => {
+  humanChoice = "paper";
+  playRound();
+});
+
+const scissors = document.querySelector("#Scissors");
+scissors.addEventListener("click", () => {
+  humanChoice = "scissors";
+  playRound();
+});
+
 //Get computer choice
 function getComputerChoice() {
-  let num = Math.random();
+  let num = Math.floor(Math.random() * 3);
   let computerChoice;
-  if (num > 0.6666666666) {
+  if (num ==0 ) {
     computerChoice = "rock";
-  } else if (num <= 0.666666666 && num > 0.3333333333) {
+  } else if (num == 1) {
     computerChoice = "paper";
   } else {
     computerChoice = "scissors";
   }
   return computerChoice;
 }
-
-//Get human choice
-function getHumanChoice() {
-  let humanChoice = prompt("Rock, paper or scissors?");
-  return humanChoice.toLowerCase();
-}
-
-//Declare the players score variables
-let humanScore = 0;
-let computerScore = 0;
-
-//Write the logic to play the entire game
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-
-    function playRound(humanChoice, computerChoice) {
-      if (humanChoice == "rock" && computerChoice == "scissors") {
-        humanScore++;
-      } else if (humanChoice == "scissors" && computerChoice == "paper") {
-        humanScore++;
-      } else if (humanChoice == "paper" && computerChoice == "rock") {
-        humanScore++;
-      } else {
-        computerScore++;
-      }
-    }
-  }
-  console.log("Your score: " + humanScore);
-  console.log("Computer score: " + computerScore);
-  if (humanScore > computerScore) {
-    console.log("You win!")
-  } else {
-    console.log("You lose :(")
-  }
-}
-
-console.log(playGame());
